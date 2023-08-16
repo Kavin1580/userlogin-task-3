f9e55547c077a720d064cd169ee87888c2e6501e
Kavin1580 <kavinronaldo42@gmail.com>
2023-08-16 15:37:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+    if (email === '' || password === '' || username === '') {
+      setErrorMessage('Please fill in all fields.');
+      return;
+    }
+
+    // Perform login logic here
+    // You can redirect the user to a new page or show a success message
+    // For now, let's just show an alert
+    alert(`Welcome! ${username}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <h1>Login</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className={errorMessage && username === '' ? 'error-input' : ''}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={errorMessage && email === '' ? 'error-input' : ''}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className={errorMessage && password === '' ? 'error-input' : ''}
+        />
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default LoginPage;
+
